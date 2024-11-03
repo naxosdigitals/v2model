22c05a36fb3d81361f84cfef287d58f541ac3fa1
Saxon <saxon@naxosdigitals.com>
2024-11-03 11:01:59 +1000

    fetch url

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 // config.js
 export const config = {
-    fetchurl: 'http://localhost:3002/api/message',
+    fetchurl: 'https://my-express-app-861019374471.us-central1.run.app/api/message',
     origin: ['localhost', 'https://canopy.school'],
     projectid: '670051167305f4f9dfa409a7'
   };
-  
   
